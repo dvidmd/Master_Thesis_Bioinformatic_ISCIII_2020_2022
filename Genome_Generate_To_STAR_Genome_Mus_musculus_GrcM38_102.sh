df3d723182cf7b6bf2cac4c3da8d1dc37a841686
David Martinez-Delgado <96123001+dvidmd@users.noreply.github.com>
2022-01-13 18:11:26 +0100

    Update Genome_Generate_To_STAR_Genome_Mus_musculus_GrcM38_102.sh

--- Genome_Generate_To_STAR_Genome_Mus_musculus_GrcM38_102.sh
+++ Genome_Generate_To_STAR_Genome_Mus_musculus_GrcM38_102.sh
@@ -1,1 +1,3 @@
+# This script was used to create the STAR genome index of Mus musculus genome (GrcM39 v102): 
+
 STAR --runThreadN 4 --runMode genomeGenerate --genomeDir GenomeGrcM38 --genomeFastaFiles Mus_musculus.GRCm38.dna.primary_assembly.fa --sjdbGTFfile Mus_musculus.GRCm38.102.gtf --sjdbOverhang 100
